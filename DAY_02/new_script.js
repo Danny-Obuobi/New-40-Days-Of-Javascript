0d9d6926f6afa89ef0532f9600a2ca8bd76c2e34
Daniel Adu Obuobi <adudaniel131@gmail.com>
2025-02-18 13:23:45 +0000

    Modified the values of the object and arrays in Task 4 in the JS file

---
+++ DAY_02/new_script.js
@@ -0,0 +1,47 @@
+// Task 1
+
+let my_Name, new_age, Is_student_status, favorite_language;
+
+my_Name = "Daniel Feltz";
+new_age = 30;
+Is_student_status = true;
+favorite_language = "Javascript";
+
+// Task 2
+
+console.log(my_Name, new_age, Is_student_status, favorite_language);
+
+// Task 3 
+let my_Car = "Range Rover";
+my_Car = "Benz";
+console.log(my_Car);
+
+// const b = 70;
+// b = 68;
+// console.log(b);
+
+// Task 4 
+
+let my_Object = {
+    name: "Nikesh Arora",
+    age: 50,
+    city: "Boston"
+};
+
+console.log(my_Object);
+
+let second_item = {
+    book: "Money Trap",
+    author : "Alok Sama"
+}
+
+console.log(second_item);
+
+
+let first_arr = [3, 4, 5, 6];
+console.log(first_arr);
+
+let second_arr = first_arr;
+second_arr = [20, 40, 50, 60];
+console.log(second_arr);
+
